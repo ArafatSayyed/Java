df536e740cbb4d4fba538acfa5b007b310543b50
Arafat Sayyed <arafatsayyed07@gmail.com>
2025-08-18 00:10:33 +0530

    Add files via upload

---
+++ ArithmeticOperationInt.java
@@ -0,0 +1,13 @@
+package Basic;
+
+public class ArithmeticOperationInt {
+    public static void main(String[] args) {
+        double x = 5;
+        double y = 2;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+
+}
