dc67a409e2ac7cbd52848cab8392bde45ea2e39c
Arafat Sayyed <arafatsayyed07@gmail.com>
2025-08-18 00:12:59 +0530

    Add files via upload

---
+++ DivisionOfDataTypes.java
@@ -0,0 +1,13 @@
+public class DivisionOfDataTypes {
+    public static void main(String[] args) {
+        double x = 37;
+        double y = 6;
+        System.out.println(x/y);
+        int a = 27;
+        int b = 5;
+        System.out.println(a/b);
+
+        System.out.println(x/b);
+        System.out.println(a/y);
+    }
+}
