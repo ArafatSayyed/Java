24397a5fc051669d7e40acdbb8ae5c4981d389e9
Arafat Sayyed <arafatsayyed07@gmail.com>
2025-08-19 01:24:01 +0530

    Add files via upload

---
+++ InputFromUser.java
@@ -0,0 +1,18 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class InputFromUser {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Radius: ");
+        double r = sc.nextDouble();
+        double a = 3.14 * r * r;
+        System.out.print("Area is: ");
+        System.out.println(a);
+
+
+    }
+
+
+}
