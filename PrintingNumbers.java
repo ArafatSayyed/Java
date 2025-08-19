f1929c5a0c2d5961b8fa9773863b6ff261f75884
Arafat Sayyed <arafatsayyed07@gmail.com>
2025-08-20 01:41:25 +0530

    Add files via upload

---
+++ PrintingNumbers.java
@@ -0,0 +1,11 @@
+package Basic;
+
+public class PrintingNumbers {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println("2");
+        System.out.println(1+2);
+        System.out.println("1+2");
+
+    }
+}
