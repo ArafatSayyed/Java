21179893eb40802846095e44b3d329c01c4e88ca
Arafat Sayyed <arafatsayyed07@gmail.com>
2025-08-20 01:42:47 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,22 @@
+package Basic;
+
+public class Variable {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x);
+        System.out.println(x+2);
+        System.out.println(x+"Arafat");
+
+        int y = 4;
+        System.out.println(y);
+        System.out.println(x+y);
+        System.out.println(x*y);
+
+        int z;
+        z=10;
+        System.out.println(z);
+        z=20;
+        System.out.println(z);
+    }
+
+}
