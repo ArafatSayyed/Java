7f5e0cd356fb972bbf67288769273b755d7f333c
Arafat Sayyed <arafatsayyed07@gmail.com>
2025-08-20 01:41:05 +0530

    Add files via upload

---
+++ ModulusOperator.java
@@ -0,0 +1,8 @@
+public class ModulusOperator {
+    public static void main(String[] args) {
+        double x = 37;
+        double y = 6;
+        double z = x % y;
+        System.out.println(z); // prints 1.0
+    }
+}
