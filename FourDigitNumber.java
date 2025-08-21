1f54d3c9a50a9d02268a7e4a710159ac758d4ce2
Arafat Sayyed <arafatsayyed07@gmail.com>
2025-08-22 00:04:22 +0530

    Add files via upload

---
+++ FourDigitNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FourDigitNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int n = sc.nextInt();
+
+        if (n>999 && n<10000)
+        {
+            System.out.println("Four Digit Number");
+        }
+        else
+        {
+            System.out.println("Not Four Digit Number");
+        }
+    }
+}
