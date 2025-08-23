bd6918633027731f55b933d05252cdbd4d8dca11
Arafat Sayyed <arafatsayyed07@gmail.com>
2025-08-23 08:48:17 +0530

    Add files via upload

---
+++ If_Else_If_ElseProfitLoss.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class If_Else_If_ElseProfitLoss {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter SP: ");
+            double sp = sc.nextDouble();
+
+            Scanner si = new Scanner(System.in);
+            System.out.print("Enter CP: ");
+            double cp = si.nextDouble();
+
+            if (sp > cp){
+                System.out.println("Profit is "+(sp - cp));
+                System.out.println("Profit Percentage is "+(((sp - cp)*100)/cp));
+            }
+            else if (sp < cp) {
+                System.out.println("Loss is "+(cp - sp));
+                System.out.println("Loss percentage is "+(((cp - sp)*100)/cp));
+            }
+            else {
+                System.out.println("No Profit, No Loss");
+            }
+        }
+}
+
+
