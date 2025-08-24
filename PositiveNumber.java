9d43cfc333228da9420dcd753a4bbfb3dfdfae96
Arafat Sayyed <arafatsayyed07@gmail.com>
2025-08-25 00:31:51 +0530

    Add files via upload

---
+++ PositiveNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PositiveNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int n = sc.nextInt();
+        if (n>=0){
+            System.out.println(n);
+        }
+        else {
+            System.out.println(-n);
+        }
+    }
+}
