57be9a9d0f6ab11e8b2de4c5562aae4581a8ed18
Arafat Sayyed <arafatsayyed07@gmail.com>
2025-08-26 00:21:42 +0530

    Add files via upload

---
+++ RelationalOperators.java
@@ -0,0 +1,15 @@
+public class RelationalOperators {
+        public static void main(String[] args) {
+            int x = 5;
+            int y = 4;
+
+            System.out.println(x==y);
+            System.out.println(x>y);
+            System.out.println(x<y);
+            System.out.println(x!=y);
+            System.out.println(x>=y);
+            System.out.println(x<=y);
+
+        }
+}
+
