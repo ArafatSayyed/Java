ed241f30d905e020e789f8e1f100c9b52df21014
Arafat Sayyed <arafatsayyed07@gmail.com>
2025-08-27 00:47:29 +0530

    Add files via upload

---
+++ ThreePositiveIntegersAsInputAndPrintTheSmallestOne.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class ThreePositiveIntegersAsInputAndPrintTheSmallestOne
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+        int s;
+        if (a <= b && a <= c)
+        {
+            s = a;
+        }
+        else if (b <= a && b <= c)
+        {
+            s = b;
+        }
+        else
+        {
+            s = c;
+        }
+        System.out.println("The smallest number is: " + s);
+    }
+}
