ff16717b0e69cd46c2c2e37c8fcdef34677b2cbf
Arafat Sayyed <arafatsayyed07@gmail.com>
2025-08-30 14:50:20 +0530

    Add files via upload

---
+++ BasicLoop.java
@@ -0,0 +1,10 @@
+public class BasicLoop
+{
+    public static void main(String[] args)
+    {
+        for (int i = 1; i<=3; i++)
+        {
+            System.out.println("Arafat");
+        }
+    }
+}
