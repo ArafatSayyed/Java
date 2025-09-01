683993f6fc8b37dc279035b1eca225a50af9ce02
Arafat Sayyed <arafatsayyed07@gmail.com>
2025-09-02 00:18:22 +0530

    Add files via upload

---
+++ EvenNumberOneToHundred.java
@@ -0,0 +1,27 @@
+//1st Method
+public class EvenNumberOneToHundred
+/*{
+    public static void main(String[] args)
+    {
+        for (int i=0;i<=100;i++)
+        {
+            if (i%2==0)
+            {
+                System.out.println(i+" ");
+            }
+        }
+    }
+}*/
+//yhe 100 baar chalaga
+
+//2nd Method
+{
+    public static void main(String[] args)
+    {
+        for (int i=0;i<=100;i=i+2)
+        {
+            System.out.print(i+ " ");
+        }
+    }
+}
+// yhe 100 baar chalaga
