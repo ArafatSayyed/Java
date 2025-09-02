680ab8446eb0f13a201469e55a6bf307c66cfcb5
Arafat Sayyed <arafatsayyed07@gmail.com>
2025-09-03 03:17:56 +0530

    Add files via upload

---
+++ nTimes.java
@@ -0,0 +1,10 @@
+public class nTimes
+{
+    public static void main(String[] args)
+    {
+        for (;;)
+        {
+            System.out.println("This will run forever!");
+        }
+    }
+}
