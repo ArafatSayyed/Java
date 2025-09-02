06d2fd81993bee2d3f32844ca5a1ebc28e51e0ad
Arafat Sayyed <arafatsayyed07@gmail.com>
2025-09-03 03:17:13 +0530

    Add files via upload

---
+++ InfiniteLoop.java
@@ -0,0 +1,16 @@
+public class InfiniteLoop
+{
+    public static void main(String[] args)
+    {
+
+        /*
+        for (int i=1; i<=10; i--)
+        {
+            System.out.println(i);
+        }*/
+        while (1<2)
+        {
+            System.out.println("Arafat");
+        }
+    }
+}
