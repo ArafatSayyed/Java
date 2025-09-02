2861e441873c4dee2e18d1b627879a473a0c740c
Arafat Sayyed <arafatsayyed07@gmail.com>
2025-09-03 03:18:11 +0530

    Add files via upload

---
+++ NumberFromNToOne.java
@@ -0,0 +1,11 @@
+public class NumberFromNToOne
+{
+    public static void main(String[] args)
+    {
+        int n = 10;
+        for (int i=n; i>=1; i--)
+        {
+            System.out.print(i + " ");
+        }
+    }
+}
