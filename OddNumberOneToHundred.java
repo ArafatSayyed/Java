893754af3a497132692cf00ea28dc917fb9945d5
Arafat Sayyed <arafatsayyed07@gmail.com>
2025-09-04 00:52:22 +0530

    Add files via upload

---
+++ OddNumberOneToHundred.java
@@ -0,0 +1,30 @@
+//1st Method
+public class OddNumberOneToHundred
+/*
+{
+    public static void main(String[] args)
+    {
+        for (int i=0;i<=100;i++)
+        {
+            if (i%2!=0)
+            {
+                System.out.println(i+" ");
+            }
+        }
+    }
+}
+//yhe 100 baar chalaga
+*/
+
+//2nd Method
+
+{
+    public static void main(String[] args)
+    {
+        for (int i=1;i<=100;i=i+2)
+        {
+            System.out.print(i+ " ");
+        }
+    }
+}
+//YHE 50 baar chalaga
